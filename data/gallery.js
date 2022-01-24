359ff6fc7278530a2847f0f3d1ab4cf524dad4c0
nivenreed <nivenreed@hotmail.com>
2022-01-24 12:35:07 +0000

    creating basic pages

---
+++ data/gallery.js
@@ -0,0 +1,16 @@
+
+
+const album = {
+    title: `Bali`,
+    description: `Trirp to Bali for Will & Erin's Wedding`,
+    images:[
+        {
+            title: `photo 1`,
+            src: `./gallery/bali/`,
+            created: `4th Oct 2017`,
+            description: `Sayiayng I Do`,
+        }
+    ]
+};
+
+
